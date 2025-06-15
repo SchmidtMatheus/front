8268261ee16b31c0b18824621c8dbdb6a53135e8
MatheusSchmidt <matheus.schmidt@outlook.com>
2025-06-15 12:09:47 -0300

    Remove unnecessary comments and clean up style imports in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script setup>
-// App-level logic can be added here if needed
 </script>
 
 <style>
@@ -11,6 +10,5 @@
 @import 'tailwindcss/components';
 @import 'tailwindcss/utilities';
 
-/* Add Material Icons */
 @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
 </style>
